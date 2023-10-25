fd3697a4854981e99a6565494acd72f9e00782fc
rakesh <rakesh@richestsoft.in>
2023-10-25 09:23:00 +0530

    chhanges

--- Client/src/Components/header/Header.jsx
+++ Client/src/Components/header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <nav className='header'>
       <div className="header__logo">
-        <Link to="/"><h3>Task Manager</h3></Link>
+        <Link to="/"><h3 style={{ textDecoration: "none" }}>Task Manager</h3></Link>
       </div>
       <div className="header__buttons" >
-        <Link to="/signin">  <button type="button">Sign In </button></Link>
-        <button type="button">Sign Out </button>
+        <Link to="/signin"><button type="button">Sign In</button></Link>
+        <Link to="/signin"><button type="button">Sign out</button></Link>
       </div>
     </nav>
   )
